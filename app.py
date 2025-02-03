4047a61224811ad3444170d4ec1d13a48ee8b217
heyaohuo9 <heyaohuo9@gmail.com>
2025-02-03 14:36:44 +0000

    a

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
 from flask import render_template
-
+from flask_restful import Resource, Api
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/t')
+
